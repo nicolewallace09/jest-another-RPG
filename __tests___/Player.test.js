bb283f87c322ce81d777c6944a978ab1bdcec26f
nicolewallace09 <nicole.elisaw@gmail.com>
2020-05-12 18:37:38 -0700

    player construct and test

---
+++ __tests___/Player.test.js
@@ -0,0 +1,21 @@
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion')
+
+console.log(new Potion());
+
+const Player = require('../lib/Player.js'); 
+
+test('creates a player object', () => {
+    const player = new Player('Dave');
+
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+    // inventory should be an array containing an object
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
+});
+
